6301820151c2b0201e0c73ca767fe8aeccac32a0
Hiago.D <hiagofdss900@gmail.com>
2024-03-05 23:03:03 -0400

    up-rep
    Upando calculator-C#

---
+++ Calculator/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        decimal value1 = 0, value2 = 0;
+        string operation = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            textResult.Text = textResult.Text + "0";
+;        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textResult.Text += "1";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textResult.Text += "2";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textResult.Text += "3";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textResult.Text += "4";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textResult.Text += "5";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textResult.Text += "6";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textResult.Text += "7";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textResult.Text += "8";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textResult.Text += "9";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textResult.Text += ".";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string button7_Click(object sender, EventArgs e)
+        {
+            value1 = decimal.Parse (textResult.Text, CultureInfo.InvariantCulture);
+            textResult.Text = "";
+            operation = "Soma";
+            lblOperation.Text = "+"
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.WriteLine("Seja Bem-vindo a minha Calculadora!!!");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Qual operação deseja fazer?");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
+
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Digite sua opção: ");
+
+            short res = short.Parse(Console.ReadLine());
+
+            switch (res)
+            {
+                case 1 : Soma(); break;
+                case 2 : Subtracao(); break;
+                case 3 : Divisao(); break;
+                case 4 : Multiplicacao(); break;
+                case 5 : System.Environment.Exit(0); break;
+                default : Erro(); break;
+
+            }
+        }
+        static void Soma()
+        {
+            Console.Clear();
+            
+            Console.WriteLine("Primeiro valor: ");
+            float v1= float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+            Console.WriteLine("O resultado da soma é " + resultado);
+            Console.ReadKey();
+            Menu();
+        }
+        static void Subtracao()
+        {
+            Console.Clear();
+            
+            Console.WriteLine("Primeiro valor: ");
+            float v1= float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}" );
+            Console.ReadKey();
+            Menu();
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+            
+            Console.WriteLine("Primeiro valor: ");
+            float v1= float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão é {resultado}" );
+            Console.ReadKey();
+            Menu();
+        } 
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            
+            Console.WriteLine("Primeiro valor: ");
+            float v1= float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("");
+
+            float resultado = v1 * v2;
+            Console.WriteLine($"O resultado da multiplicação é {resultado}" );
+            Console.ReadKey();
+            Menu();
+        }
+        static void Erro()
+        {
+            Console.Clear();
+            Console.WriteLine("Nenhuma das opções foi selecionada!!!");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Deseja voltar ao menu?");
+
+            Console.WriteLine("-------------------------");
+
+            Console.WriteLine("1 - Sim!");
+            Console.WriteLine("2 - Não!");
+            
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite sua opção: ");
+            float selc = float.Parse(Console.ReadLine());
+
+            switch (selc)
+            {
+                case 1 : Menu(); break;
+                case 2 : Environment.Exit(0); break;
+                default : Menu(); break;
+            }
+
+
+
+        }
+   }
+}
